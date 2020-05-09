fdfd28db34b8a044f2adad20c1afb204320c9b2e
Weng Xuetian <wengxt@gmail.com>
2020-05-08 17:06:42 -0700

    update license.

--- test/lua/testlua/test.lua
+++ test/lua/testlua/test.lua
@@ -1,7 +1,7 @@
 --
 -- SPDX-FileCopyrightText: 2020 Weng Xuetian <wengxt@gmail.com>
 --
--- SPDX-License-Identifier: GPL-2.0-or-later
+-- SPDX-License-Identifier: LGPL-2.1-or-later
 --
 local fcitx = require("fcitx")
 
