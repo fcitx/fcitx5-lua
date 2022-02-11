fb8f9d08f1f3a3abc1d5d7639fa8a285dd2256a9
Potato Hatsue <1793913507@qq.com>
2022-02-10 21:18:21 -0500

    Remove unused Helper.cmake; Add option ENABLE_TEST

--- cmake/Helper.cmake
+++
@@ -1,9 +0,0 @@
-
-function(file2cstring tgt_name ifile)
-  get_filename_component(ifile "${ifile}" ABSOLUTE)
-  set(output_file "${CMAKE_CURRENT_BINARY_DIR}/${tgt_name}.h")
-  add_custom_command(OUTPUT "${output_file}"
-    COMMAND file2cstring "${tgt_name}" "${ifile}" > "${output_file}"
-    DEPENDS file2cstring ${ifile})
-  add_custom_target("${tgt_name}" ALL DEPENDS "${output_file}")
-endfunction()
